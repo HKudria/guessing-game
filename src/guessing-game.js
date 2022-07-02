56f8acb73d827b867d82d545ef98b4e04bed47cf
Herman Kudria <h.kudrya@hotmail.com>
2022-07-02 17:26:42 +0200

    feat: add game logic

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,49 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+       return this.guessNumber=Math.round(this.min + (this.max - this.min)/2)
     }
 
     lower() {
-
+       this.max=this.guessNumber
     }
 
     greater() {
-
+        this.min=this.guessNumber
     }
 }
+
+// const game = new GuessingGame();
+// game.setRange(0, 4048)
+//
+// let result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.greater();
+// result = game.guess();
+// game.lower();
+// result = game.guess();
+// console.log(result)
 
 module.exports = GuessingGame;
